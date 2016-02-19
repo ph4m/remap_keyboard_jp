eea1ca5b7af9e2c6101729b33d9ee4eb0ce4ab85
Tu-Hoa Pham <thp@pham.in>
2016-02-19 13:48:31 +0900

    Exit gracefully

--- add_xkb_config.sh
+++ add_xkb_config.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 PATHSCRIPT=$(readlink -f "$0")
 PATHDIR=$(dirname "${PATHSCRIPT}")
 PATHCONFIG="${PATHDIR}/xkb"
-echo $PATHCONFIG
 
 setxkbmap -print | sed -e '/xkb_keycodes/s/"[[:space:]]/+local&/' \
                  | sed -e '/xkb_symbols/s/"[[:space:]]/+local&/' \
                  | xkbcomp -I${PATHCONFIG} - $DISPLAY
+
+exit 0
